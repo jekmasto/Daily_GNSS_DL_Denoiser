ccc849ef4076f47b3e70e1d8e58863493f132171
Giacomo <76777270+jekmasto@users.noreply.github.com>
2023-07-20 17:52:31 +0200

    Update Download_data.py

--- Prepare_data/Download_data.py
+++ Prepare_data/Download_data.py
@@ -1,1 +1,10 @@
-dd
+from Nevada_download import *
+
+# Saving folder
+saving_cd_coord = "/Users/giacomo/Documents/PhD/Papers/GNSS_DENOISER/Chile"
+############ COORDINATES ############
+lat_south=-55
+lat_north=-5
+lon_west=-81
+lon_east= -66
+import_stations(saving_cd_coord,lat_south,lat_north,lon_west,lon_east)
